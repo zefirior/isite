2003d070cb84ab0040bb1b4f30958de24855d240
Daniil Galiev <dgaliev@rc-online.org>
2018-02-05 20:32:39 +0500

    keyword

---
+++ deploy_flask.sh
@@ -0,0 +1,3 @@
+# ~/public_html/work пробовал -> не то. 
+# команда отрабатывает перенос директории ~/work/git/isite/work/ в ~/public_html/work
+scp -Cr ~/work/git/isite/flaskapp/ insidearru@77.222.56.213:~/public_html;
